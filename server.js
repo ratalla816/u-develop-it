d2b3de31117ea13699453f795d159ecba7cce2b9
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-12 18:50:02 -0500

    create candidate

--- server.js
+++ server.js
@@ -23,6 +23,26 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+  // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
 // GET a single candidate
 // db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
 //     if (err) {
