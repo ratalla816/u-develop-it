576c059a20630a2c859b4e0ec5b0beb68dffe4e5
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-11 21:08:43 -0500

    connected to election database

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 // const db = require('./db/connection');
-// const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// // Express middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// // Use apiRoutes
-// app.use('/api', apiRoutes);
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'root',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
-// // Start server after DB connection
-// db.connect(err => {
-//   if (err) throw err;
-//   console.log('Database connected.');
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
-    // Default response for any other request (Not Found)
-    app.use((req, res) => {
-        res.status(404).end();
-      });
-  
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
