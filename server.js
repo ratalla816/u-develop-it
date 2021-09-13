0faae25e3f457ef3b2ca3a59bbd3f6b4f2602868
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-12 20:30:08 -0500

    add post rte

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 // const db = require('./db/connection');
 
@@ -23,40 +24,101 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
-// Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-
-  // Delete a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(result);
-//   });
+  // GET all the candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    
+db.query(sql, (err, rows) => {
+        if(err){
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
+});
 
 // GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(row);
-//   });
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
   
-//  This method runs the SQL query and executes the callback with all the resulting rows that match the query. 
-// generates an array with all the candidates
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-//   });
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
 
+
+  // Delete a candidate
+//   The endpoint used here also includes a route parameter to uniquely identify the candidate to remove. 
+  app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+//   we're using a prepared SQL statement with a placeholder. We'll assign the req.params.id to params 
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+// What if the user tries to delete a candidate that doesn't exist? That's where the else if statement comes in. 
+// If there are no affectedRows as a result of the delete query, that means that there was no candidate by that id. 
+// Therefore, we should return an appropriate message to the client, such as "Candidate not found".        
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+//   The JSON object route response will be the message "deleted", with the changes property set to result.affectedRows. Again, this will verify whether any rows were changed.          
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
+
+  // Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(
+      body,
+      'first_name',
+      'last_name',
+      'industry_connected'
+    );
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+
+//   Database call
+// there is no column for the id. MySQL will autogenerate the id and relieve us of the responsibility to know which id is available to populate.
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+//  The params assignment contains three elements in its array that contains the user data collected in req.body.
+const params = [body.first_name, body.last_name, body.industry_connected];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    message: 'success',
+    data: body
+   });
+  });
+});
+    
+  
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
